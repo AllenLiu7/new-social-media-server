808108ef35df84f14a60eecc4fc7cfe19ea414e5
Allen Liu <allen6qi@gmail.com>
2021-08-11 10:56:21 +0800

    added route for posting image

--- app.js
+++ app.js
@@ -2,20 +2,44 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const multer = require('multer');
 const userRouter = require('./routes/user.routers');
 const authRouter = require('./routes/auth.routers');
 const postRouter = require('./routes/post.routers');
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('combined'));
-
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
+
+//upload image
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post(
+  '/api/post_image',
+  upload.single('uploaded_file'),
+  function (req, res, next) {
+    try {
+      return res.status(200).json('File uploded successfully');
+    } catch (error) {
+      next(createError(500, err));
+    }
+  }
+);
 
 //Custom express error handler
 app.use((error, req, res, next) => {
