5b08fcc68ea9ce5d3b629850dd6bae759e981fe3
Allen Liu <allen6qi@gmail.com>
2021-08-12 10:53:51 +0800

    added /assets route to serve assets

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 //upload image
 const storage = multer.diskStorage({
@@ -32,17 +33,13 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post(
-  '/api/post_image',
-  upload.single('file'),
-  function (req, res, next) {
-    try {
-      return res.status(200).json('File uploded successfully');
-    } catch (error) {
-      next(createError(500, err));
-    }
+app.post('/api/post_image', upload.single('file'), function (req, res, next) {
+  try {
+    return res.status(200).json('File uploded successfully');
+  } catch (error) {
+    next(createError(500, err));
   }
-);
+});
 
 //Custom express error handler
 app.use((error, req, res, next) => {
