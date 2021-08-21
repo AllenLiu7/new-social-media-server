ddba2306ef3f77df7d36669874b05ee8721042c4
Allen Liu <allen6qi@gmail.com>
2021-08-21 11:09:55 +0800

    updated multer middleware to define filename at server and respone with the file name

--- app.js
+++ app.js
@@ -23,23 +23,30 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/assets/post');
   },
+
   filename: (req, file, cb) => {
-    cb(null, req.body.name);
+    //if you want to set file name at front end, just cb(null, req.body.name)
+    //the method will create bug when using with postman
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
   },
-  // filename: (req, file, cb) => {
-  //   //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-  //   cb(null, file.originalname);
-  // },
 });
 
 const upload = multer({ storage: storage });
-app.post('/api/post_image', upload.single('file'), function (req, res, next) {
-  try {
-    return res.status(200).json('File uploded successfully');
-  } catch (error) {
-    next(createError(500, err));
+app.post(
+  '/api/post_image',
+  upload.single('post-image'), //this is the feild name for the file
+  function (req, res, next) {
+    try {
+      return res.status(200).json({
+        message: 'File uploded successfully',
+        fileName: req.file.filename, //return the file name to frontend for handling
+      });
+    } catch (error) {
+      next(createError(500, err));
+    }
   }
-});
+);
 
 //Custom express error handler
 app.use((error, req, res, next) => {
