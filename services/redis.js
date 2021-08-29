89bad1e61f57627a00e0aead95714725014ac830
Allen Liu <allen6qi@gmail.com>
2021-08-29 21:38:40 +0800

    modified redis config

--- services/redis.js
+++ services/redis.js
@@ -1,20 +1,29 @@
 const redis = require('redis');
 
-const createClient = () => {
-  const client = redis
-    .createClient({
-      port: 14042,
-      host: 'redis-14042.c1.asia-northeast1-1.gce.cloud.redislabs.com',
-      password: 'Fbml8T86HBxHKtPF6ddSRgt22lDCzpTE',
-    })
-    .on('connect', function () {
-      console.log('Redis connection ready!');
-    })
-    .on('error', (err) => {
-      console.error(err);
-    });
+const client = redis.createClient({
+  port: 14042,
+  host: 'redis-14042.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+  password: 'Fbml8T86HBxHKtPF6ddSRgt22lDCzpTE',
+});
 
-  return client;
-};
+client.on('connect', () => {
+  console.log('Client connected to redis...');
+});
 
-module.exports = createClient;
+client.on('ready', () => {
+  console.log('Client connected to redis and ready to use...');
+});
+
+client.on('error', (err) => {
+  console.log(err.message);
+});
+
+client.on('end', () => {
+  console.log('Client disconnected from redis');
+});
+
+process.on('SIGINT', () => {
+  client.quit();
+});
+
+module.exports = client;
